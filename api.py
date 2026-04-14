5eebd5ad0f53973a870a3c933d4bf655f23585a0
peacemaker3050-ux <peacemaker3050@gmail.com>
2026-04-14 09:22:32 -0700

    Add files via upload

--- api.py
+++ api.py
@@ -34,9 +34,9 @@ DRIVE_FOLDER_ID = "1T0MwUb-dc3UN3hMjrio1GVT6lm1mQl4Q"
 # Google Drive OAuth credentials (from bot.py)
 CLIENT_ID     = '1006485502608-ok2u5i6nt6js64djqluithivsko4mnom.apps.googleusercontent.com'
 CLIENT_SECRET = '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'
-REFRESH_TOKEN = '1//03avR5DnVX4uvCgYIARAAGAMSNwF-L9IrNNGLQIN2Uu8IuIAFye3n6YJYm_HszZwMJNp4lMhyTlpHTH3GaxR2YKKjTC6TzTxv4zE'
+REFRESH_TOKEN = '1//03tt9LkYllqPGCgYIARAAGAMSNwF-L9Ir5WqaeOyHBPBLEHgbih1R8eVcuF5SiIfoZnjQxYSKOFMJjtPbtkHsE1xTXbuYTmX1t5A'
 
-RAILWAY_URL = "https://web-production-b8f1c.up.railway.app"
+RAILWAY_URL = "https://web-production-ae004.up.railway.app"
 
 # ==========================================
 # 2. Firebase init
